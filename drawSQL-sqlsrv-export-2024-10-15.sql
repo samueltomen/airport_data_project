5567694f9d3b879f618b491981e81a9b8b158771
unknown <lucas.tara960@gmail.com>
2024-10-15 17:27:38 +0200

    init database

---
+++ drawSQL-sqlsrv-export-2024-10-15.sql
@@ -0,0 +1,78 @@
+CREATE TABLE "Planes"(
+    "id" INT NOT NULL,
+    "tailnum" VARCHAR(255) NOT NULL,
+    "year" INT NOT NULL,
+    "type" VARCHAR(255) NOT NULL,
+    "manufacturer" VARCHAR(255) NOT NULL,
+    "model" VARCHAR(255) NOT NULL,
+    "engines" INT NOT NULL,
+    "seats" INT NOT NULL,
+    "speed" INT NOT NULL,
+    "engine" VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "Planes" ADD CONSTRAINT "planes_id_primary" PRIMARY KEY("id");
+CREATE TABLE "Weather"(
+    "id" INT NOT NULL,
+    "origin" VARCHAR(255) NOT NULL,
+    "year" INT NOT NULL,
+    "month" INT NOT NULL,
+    "day" INT NOT NULL,
+    "hour" INT NOT NULL,
+    "temp" FLOAT(53) NOT NULL,
+    "dewp" FLOAT(53) NOT NULL,
+    "humid" FLOAT(53) NOT NULL,
+    "wind_dir" INT NOT NULL,
+    "wind_speed" FLOAT(53) NOT NULL,
+    "wind_gust" FLOAT(53) NOT NULL,
+    "precip" FLOAT(53) NOT NULL,
+    "pressure" FLOAT(53) NOT NULL,
+    "visib" FLOAT(53) NOT NULL,
+    "time_hour" TIMESTAMP NOT NULL
+);
+ALTER TABLE
+    "Weather" ADD CONSTRAINT "weather_id_primary" PRIMARY KEY("id");
+CREATE TABLE "AIrlines"(
+    "carrier" CHAR(255) NOT NULL,
+    "name" CHAR(255) NOT NULL
+);
+ALTER TABLE
+    "AIrlines" ADD CONSTRAINT "airlines_carrier_primary" PRIMARY KEY("carrier");
+CREATE TABLE "Airports"(
+    "id" BIGINT NOT NULL,
+    "faa" VARCHAR(255) NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "lat" FLOAT(53) NOT NULL,
+    "lon" FLOAT(53) NOT NULL,
+    "alt" INT NOT NULL,
+    "tz" INT NOT NULL,
+    "dst" VARCHAR(255) NOT NULL,
+    "tzone" VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "Airports" ADD CONSTRAINT "airports_id_primary" PRIMARY KEY("id");
+CREATE TABLE "Flights"(
+    "id" INT NOT NULL,
+    "year" INT NOT NULL,
+    "month" INT NOT NULL,
+    "day" INT NOT NULL,
+    "dep_time" INT NOT NULL,
+    "sched_dep_time" INT NOT NULL,
+    "dep_delay" INT NOT NULL,
+    "arr_time" INT NOT NULL,
+    "sched_arr_time" INT NOT NULL,
+    "arr_delay" INT NOT NULL,
+    "carrier" VARCHAR(255) NOT NULL,
+    "flight" INT NOT NULL,
+    "tailnum" VARCHAR(255) NOT NULL,
+    "origin" VARCHAR(255) NOT NULL,
+    "dest" VARCHAR(255) NOT NULL,
+    "air_time" INT NOT NULL,
+    "distance" INT NOT NULL,
+    "hour" INT NOT NULL,
+    "minute" INT NOT NULL,
+    "time_hour" TIMESTAMP NOT NULL,
+    "plane_id" INT NOT NULL
+);
+ALTER TABLE
+    "Flights" ADD CONSTRAINT "flights_id_primary" PRIMARY KEY("id");
