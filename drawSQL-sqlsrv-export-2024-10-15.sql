2966347e28f70afd4232609283d1f216f0cb9bd4
stomen <113606314+samueltomen@users.noreply.github.com>
2024-10-16 12:34:24 +0200

    import & insert flight and airports data

--- drawSQL-sqlsrv-export-2024-10-15.sql
+++ drawSQL-sqlsrv-export-2024-10-15.sql
@@ -1,5 +1,4 @@
-CREATE TABLE "Planes"(
-    "id" INT NOT NULL,
+CREATE TABLE "plane"(
     "tailnum" VARCHAR(255) NOT NULL,
     "year" INT NOT NULL,
     "type" VARCHAR(255) NOT NULL,
@@ -7,13 +6,13 @@ CREATE TABLE "Planes"(
     "model" VARCHAR(255) NOT NULL,
     "engines" INT NOT NULL,
     "seats" INT NOT NULL,
-    "speed" INT NOT NULL,
+    "speed" INT,
     "engine" VARCHAR(255) NOT NULL
 );
 ALTER TABLE
-    "Planes" ADD CONSTRAINT "planes_id_primary" PRIMARY KEY("id");
-CREATE TABLE "Weather"(
-    "id" INT NOT NULL,
+    "plane" ADD CONSTRAINT "planes_id_primary" PRIMARY KEY("tailnum");
+CREATE TABLE "weather"(
+    "id" INT NOT NULL IDENTITY(1,1),
     "origin" VARCHAR(255) NOT NULL,
     "year" INT NOT NULL,
     "month" INT NOT NULL,
@@ -31,15 +30,14 @@ CREATE TABLE "Weather"(
     "time_hour" TIMESTAMP NOT NULL
 );
 ALTER TABLE
-    "Weather" ADD CONSTRAINT "weather_id_primary" PRIMARY KEY("id");
-CREATE TABLE "AIrlines"(
+    "weather" ADD CONSTRAINT "weather_id_primary" PRIMARY KEY("id");
+CREATE TABLE "airline"(
     "carrier" CHAR(255) NOT NULL,
     "name" CHAR(255) NOT NULL
 );
 ALTER TABLE
-    "AIrlines" ADD CONSTRAINT "airlines_carrier_primary" PRIMARY KEY("carrier");
-CREATE TABLE "Airports"(
-    "id" BIGINT NOT NULL,
+    "airline" ADD CONSTRAINT "airlines_carrier_primary" PRIMARY KEY("carrier");
+CREATE TABLE "airport"(
     "faa" VARCHAR(255) NOT NULL,
     "name" VARCHAR(255) NOT NULL,
     "lat" FLOAT(53) NOT NULL,
@@ -50,29 +48,30 @@ CREATE TABLE "Airports"(
     "tzone" VARCHAR(255) NOT NULL
 );
 ALTER TABLE
-    "Airports" ADD CONSTRAINT "airports_id_primary" PRIMARY KEY("id");
-CREATE TABLE "Flights"(
-    "id" INT NOT NULL,
-    "year" INT NOT NULL,
-    "month" INT NOT NULL,
-    "day" INT NOT NULL,
-    "dep_time" INT NOT NULL,
-    "sched_dep_time" INT NOT NULL,
-    "dep_delay" INT NOT NULL,
-    "arr_time" INT NOT NULL,
-    "sched_arr_time" INT NOT NULL,
-    "arr_delay" INT NOT NULL,
-    "carrier" VARCHAR(255) NOT NULL,
-    "flight" INT NOT NULL,
-    "tailnum" VARCHAR(255) NOT NULL,
-    "origin" VARCHAR(255) NOT NULL,
-    "dest" VARCHAR(255) NOT NULL,
-    "air_time" INT NOT NULL,
-    "distance" INT NOT NULL,
-    "hour" INT NOT NULL,
-    "minute" INT NOT NULL,
-    "time_hour" TIMESTAMP NOT NULL,
-    "plane_id" INT NOT NULL
+    "airport" ADD CONSTRAINT "airports_id_primary" PRIMARY KEY("faa");
+CREATE TABLE "flight"(
+     "id" INT NOT NULL IDENTITY(1,1),
+     "year" INT NOT NULL,
+     "month" INT NOT NULL,
+     "day" INT NOT NULL,
+     "dep_time" INT NOT NULL,
+     "sched_dep_time" INT NOT NULL,
+     "dep_delay" INT NOT NULL,
+     "arr_time" INT NOT NULL,
+     "sched_arr_time" INT NOT NULL,
+     "arr_delay" INT NOT NULL,
+     "carrier" VARCHAR(255) NOT NULL,
+     "flight" INT NOT NULL,
+     "tailnum" VARCHAR(255) NOT NULL,
+     "origin" VARCHAR(255) NOT NULL,
+     "dest" VARCHAR(255) NOT NULL,
+     "air_time" INT NOT NULL,
+     "distance" INT NOT NULL,
+     "hour" INT NOT NULL,
+     "minute" INT NOT NULL,
+     "time_hour" TIMESTAMP NOT NULL
 );
 ALTER TABLE
-    "Flights" ADD CONSTRAINT "flights_id_primary" PRIMARY KEY("id");
+    "flight" ADD CONSTRAINT "flights_id_primary" PRIMARY KEY("id");
+-- ALTER TABLE
+--     "flight" ADD CONSTRAINT "flights_tailnum_foreign" FOREIGN KEY("tailnum") REFERENCES "plane"("tailnum");
